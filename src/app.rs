d772e69a35b5a2f2871680e08de5588ca1f73d3c
ezlmt <2876187491@qq.com>
2026-02-20 21:53:23 -0800

    feat: connect pgsql

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,12 @@
 use axum::{routing::get, Router};
+use sqlx::PgPool;
 
 async fn ping() -> &'static str {
     "ok"
 }
 
-pub fn create_app() -> Router {
-    Router::new().route("/ping", get(ping))
+pub fn create_app(pool: PgPool) -> Router {
+    Router::new()
+        .route("/ping", get(ping))
+        .with_state(pool)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::SocketAddr;
+use sqlx::postgres::PgPoolOptions;
 use tracing::info;
 
 mod app;
@@ -13,8 +14,21 @@ async fn main() {
     // init log
     tracing_subscriber::fmt::init();
 
+    // read db url
+    let database_url =
+        std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // create link pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to database");
+
+    info!("Database connected successfully");
+
     // construct app
-    let app = app::create_app();
+    let app = app::create_app(pool);
 
     // set the port
     let addr = SocketAddr::from(([0, 0, 0, 0], 9916));
